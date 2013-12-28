cb2f0ac93a5af1a1cea0c1255f13c162a5671262
Matt Swensen <mjs@lucidchart.com>
2013-12-27 20:57:35 -0600

    Issue #11: Browser cache management.

--- js/src/main.js
+++ js/src/main.js
@@ -1,13 +1,3 @@
-require.config({
-  baseUrl: 'js/src/',
-  paths: {
-    jquery: '../lib/jquery-2.0.3.min',
-    underscore: '../lib/amdjs-underscore-min',
-    backbone: '../lib/amdjs-backbone-min',
-    moment: '../lib/moment-2.4.0.min'
-  }
-});
-
 require(['App'], function(App) {
   new App();
 });
